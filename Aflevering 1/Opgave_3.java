17684ab40cbb37875b13e668857dcf274b8ef89b
maxpetersen2310-gif <max.petersen2310@gmail.com>
2025-09-16 11:23:17 +0200

    Opgave 3 Max

--- Aflevering 1/Opgave_3.java
+++ Aflevering 1/Opgave_3.java
@@ -1,11 +1,13 @@
 public class Opgave_3 {
-    public static final int LINES = 3;
+    public static final int LINES = 2;
     public static void main(String[] args) {
-        for (int i = 1; i <= LINES ; i++) {
-            for (int j = 1; j <= LINES; j++) {
-        System.out.print("+-");
+        for (int l=1; l<=3; l++){
+            for (int i = 1; i <= LINES ; i++) {
+                for (int j = 1; j <= LINES; j++) {
+                    System.out.print("+-");
+                }
+            }
+                System.out.println("+");
         }
-}
-System.out.println("+");
     }
 }
