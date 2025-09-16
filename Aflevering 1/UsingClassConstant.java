be7b29a70f04b5df983e0e5efe57d3b8b3d268c7
maxpetersen2310-gif <max.petersen2310@gmail.com>
2025-09-16 11:34:45 +0200

    final

--- Aflevering 1/UsingClassConstant.java
+++ Aflevering 1/UsingClassConstant.java
@@ -1,16 +1,12 @@
 public class UsingClassConstant {
-    public static final int SYMBOLS = 2;
+    public static final int SYMBOLS = 4;
     public static final int LINES = 3;
     public static void main(String[] args) {
         for (int l=1; l<=LINES; l++){
-            for (int i = 1; i <= SYMBOLS ; i++) {
-                for (int j = 1; j <= SYMBOLS; j++) {
-                    System.out.print("+-");
-                }
+            for (int j = 1; j <= SYMBOLS; j++) {
+                System.out.print("+-");
             }
                 System.out.println("+");
         }
     }
 }
-
-// Secret message
