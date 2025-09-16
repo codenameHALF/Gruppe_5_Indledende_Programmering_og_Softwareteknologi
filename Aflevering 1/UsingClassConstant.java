8e5b7ffcda319327e93e7b15afe8117daf022a9f
maxpetersen2310-gif <max.petersen2310@gmail.com>
2025-09-16 11:31:07 +0200

    lines

--- Aflevering 1/UsingClassConstant.java
+++ Aflevering 1/UsingClassConstant.java
@@ -1,9 +1,10 @@
 public class UsingClassConstant {
-    public static final int LINES = 2;
+    public static final int SYMBOLS = 2;
+    public static final int LINES = 3;
     public static void main(String[] args) {
-        for (int l=1; l<=3; l++){
-            for (int i = 1; i <= LINES ; i++) {
-                for (int j = 1; j <= LINES; j++) {
+        for (int l=1; l<=LINES; l++){
+            for (int i = 1; i <= SYMBOLS ; i++) {
+                for (int j = 1; j <= SYMBOLS; j++) {
                     System.out.print("+-");
                 }
             }
