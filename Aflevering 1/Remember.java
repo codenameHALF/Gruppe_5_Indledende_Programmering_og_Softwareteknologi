238b83429e84ce5d59ce836bb1771c61a06bcb43
MikkelFindDTU <s255468@dtu.dk>
2025-09-16 11:34:04 +0200

    remember nu med kommentarer

--- Aflevering 1/Remember.java
+++ Aflevering 1/Remember.java
@@ -1,9 +1,11 @@
 public class Remember {
+    // her kalder vi vores tekst() metode to gange og printer "remember"mellem
     public static void main(String[] args) {
         tekst();
         System.out.println("Remember:");
         tekst();
     }
+    // en metode der printer den ønskede tekst.
     public static void tekst(){
         System.out.println("Use \"\\\\\" to obtain a \'backslash\' character.");
     }
