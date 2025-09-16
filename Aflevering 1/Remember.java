25c6980c4807075d91cfc5da8984148bbbdd7990
MikkelFindDTU <s255468@dtu.dk>
2025-09-16 11:35:19 +0200

    fejl rettet2

--- Aflevering 1/Remember.java
+++ Aflevering 1/Remember.java
@@ -1,11 +1,11 @@
 public class Remember {
-    // her kalder vi vores tekst() metode to gange og printer "remember" mellem.
+    // Her kalder vi vores tekst() metode to gange og printer "remember" mellem.
     public static void main(String[] args) {
         tekst();
         System.out.println("Remember:");
         tekst();
     }
-    // en metode der printer den ønskede tekst.
+    // En metode der printer den ønskede tekst.
     public static void tekst(){
         System.out.println("Use \"\\\\\" to obtain a \'backslash\' character.");
     }
