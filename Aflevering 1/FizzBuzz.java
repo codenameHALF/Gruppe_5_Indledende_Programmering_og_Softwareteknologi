1a20f42b0e7f94d56e8d56927e9b7a3c34382f4e
maxpetersen2310-gif <max.petersen2310@gmail.com>
2025-09-16 11:56:35 +0200

    fizzbuzz

---
+++ Aflevering 1/FizzBuzz.java
@@ -0,0 +1,10 @@
+public class FizzBuzz {
+    public static final int TAL = 30;
+    public static void main(String[] args) {
+        for (int i=1; i<=TAL; i++){
+            boolean fizz = (i % 3 == 0);
+            boolean buzz = (i % 5 == 0); 
+        }
+    }
+}
+            
--- Aflevering 1/Opgave_4.java
+++
@@ -1,3 +0,0 @@
-public class Opgave_4 {
-    Hello
-}
