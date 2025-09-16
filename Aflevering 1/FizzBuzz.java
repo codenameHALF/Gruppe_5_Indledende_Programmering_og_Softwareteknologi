583e24dc9f10d5037c75ebd669da66af3b34eda3
Halfdan <halfdaborg@gmail.com>
2025-09-16 12:10:08 +0200

    Added assignment 2

--- Aflevering 1/FizzBuzz.java
+++ Aflevering 1/FizzBuzz.java
@@ -2,9 +2,37 @@ public class FizzBuzz {
     public static final int TAL = 30;
     public static void main(String[] args) {
         for (int i=1; i<=TAL; i++){
-            boolean fizz = (i % 3 == 0);
-            boolean buzz = (i % 5 == 0); 
+            //isFizzbuzz(i) ? fizzbuzz() : (isBuzz(i) ? buzz() : (isFizz(i) ? fizz() : print(String.valueOf(i))));
+            System.out.println(isFizzbuzz(i));
         }
+    }
+
+    public static void print(String output) {
+        System.out.println(output);
+    }
+
+    public static void fizz() {
+        System.out.println("fizz");
+    }
+    
+    public static void buzz() {
+        System.out.println("buzz");
+    }
+
+    public static void fizzbuzz() {
+        System.out.println("fizzbuzz");
+    }
+
+    public static boolean isFizz(int i) {
+        return i % 3 == 0;
+    }
+    
+    public static boolean isBuzz(int i) {
+        return i % 5 == 0;
+    }
+
+    public static boolean isFizzbuzz(int i) {
+        return i % 15 == 0;
     }
 }
             
---
+++ Aflevering 1/UltimateQuestion.java
@@ -0,0 +1,8 @@
+ public class UltimateQuestion {
+    // This function uses the magic number x and uses it to evaluate the ultimate question
+    public static void main(String[] args) {
+        // This is the magic number x
+        double x = 7.2;
+        System.out.println(1 + 3 + x + x + x + x + x + 5 / 4 * 2);
+    }
+}
