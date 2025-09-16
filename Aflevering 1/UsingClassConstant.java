73555fe28ad9f21ab8018253b719a292c0264f0a
Halfdan <halfdaborg@gmail.com>
2025-09-16 11:49:45 +0200

    Added comments to UsingClassConstant

--- Aflevering 1/UsingClassConstant.java
+++ Aflevering 1/UsingClassConstant.java
@@ -1,12 +1,20 @@
 public class UsingClassConstant {
+    // This constant determines how many times +- is written each line
     public static final int SYMBOLS = 4;
+    // This constant determines how many lines should be written
     public static final int LINES = 3;
+    
+    // Function for printing a square of + and -
     public static void main(String[] args) {
+        // Prints a line "LINES" times
         for (int l=1; l<=LINES; l++){
+            // Prints +- "SYMBOL" times
             for (int j = 1; j <= SYMBOLS; j++) {
+                // Prints +-
                 System.out.print("+-");
             }
-                System.out.println("+");
+            // Prints a + and a new line character at the end of the current line
+            System.out.println("+");
         }
     }
 }
