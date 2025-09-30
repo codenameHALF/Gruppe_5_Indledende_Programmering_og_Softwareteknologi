e9e87c4cf6e49bdf08573165057cb4204eaad57f
Halfdan <halfdaborg@gmail.com>
2025-09-30 11:48:40 +0200

    Added input to first assignment

--- Aflevering 2/Opgave1.java
+++ Aflevering 2/Opgave1.java
@@ -1,3 +1,10 @@
+import java.util.*;
+
 public class Opgave1 {
-    
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String text = s.next();
+        s.close();
+        System.out.println(text);
+    }
 }
