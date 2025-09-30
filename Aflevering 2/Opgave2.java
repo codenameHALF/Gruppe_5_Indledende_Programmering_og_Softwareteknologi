53e2ab476e9d362ff7fd711dc9427e541fea9a35
MikkelFindDTU <s255468@dtu.dk>
2025-09-30 13:06:26 +0200

    Løst opgave 2

--- Aflevering 2/Opgave2.java
+++ Aflevering 2/Opgave2.java
@@ -1,3 +1,36 @@
+import java.util.*;
+
 public class Opgave2 {
-    
+    public static void main(String[] args) {
+        System.out.print("Enter line to check:");
+        Scanner input = new Scanner(System.in);
+
+        String text = input.nextLine();
+
+        if (palindromeCheck(text)){
+            System.out.println("\"" + text +"\" is a palindrome!");
+        } else {
+            System.out.println("Is not a palindrome");
+        }
+        
+
+    }
+    public static boolean palindromeCheck(String text){
+
+        text = text.toLowerCase();
+        text = text.replace(".", "");
+        text = text.replace(",", "");
+        text = text.replace(" ", "");
+
+        String s = "";
+
+        for (int i = 0; i < text.length(); i++){
+            
+            char r = text.charAt(i);
+            s = r + s;
+        }
+
+        return s.equals(text);
+         
+    }
 }
