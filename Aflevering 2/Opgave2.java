93c9b1868dd32187da79acb804cb2e54934c15f2
MikkelFindDTU <s255468@dtu.dk>
2025-10-03 12:03:50 +0200

    auto format

--- Aflevering 2/Opgave2.java
+++ Aflevering 2/Opgave2.java
@@ -2,18 +2,18 @@ import java.util.*;
 
 public class Opgave2 {
     public static void main(String[] args) {
-        System.out.print("Enter line to check:");
+        System.out.print("Enter line to check: ");
         Scanner input = new Scanner(System.in);
         String text = input.nextLine();
         input.close();
 
-        if (palindromeCheck(text)){
-            System.out.println("\"" + text +"\" is a palindrome!");
+        if (palindromeCheck(text)) {
+            System.out.println("\"" + text + "\" is a palindrome!");
         } else {
             System.out.println("Is not a palindrome");
         }
     }
-    public static boolean palindromeCheck(String text){
+    public static boolean palindromeCheck(String text) {
 
         text = text.toLowerCase();
         text = text.replace(".", "");
@@ -22,13 +22,13 @@ public class Opgave2 {
 
         String s = "";
 
-        for (int i = 0; i < text.length(); i++){
-            
+        for (int i = 0; i < text.length(); i++) {
+
             char r = text.charAt(i);
             s = r + s;
         }
 
         return s.equals(text);
-         
+
     }
 }
--- Aflevering 2/Opgave4.java
+++ Aflevering 2/Opgave4.java
@@ -1,15 +1,14 @@
 import java.util.*;
 
-
 public class Opgave4 {
     public static void main(String[] args) {
-        
+
         Scanner input = new Scanner(System.in);
         Random rand = new Random();
         String prompt = "Enter grid size: ";
         int n = getInt(input, prompt);
-     
-        if (n < 0){
+
+        if (n < 0) {
             throw new IllegalArgumentException("Negative integer, Can't compute");
         }
 
@@ -17,31 +16,31 @@ public class Opgave4 {
         int y = 0;
         int steps = 0;
 
-        StdDraw.setXscale(-n-0.5, n+0.5);
-        StdDraw.setYscale(-n-0.5, n+0.5);
-        StdDraw.setPenRadius(1.0/(n*2+1));
+        StdDraw.setXscale(-n - 0.5, n + 0.5);
+        StdDraw.setYscale(-n - 0.5, n + 0.5);
+        StdDraw.setPenRadius(1.0 / (n * 2 + 1));
 
-        //System.out.println("Position = ("+ x + "," + y +")" );
+        // System.out.println("Position = ("+ x + "," + y +")" );
 
-        while (Math.abs(x) <= n && Math.abs(y) <= n){
+        while (Math.abs(x) <= n && Math.abs(y) <= n) {
 
             StdDraw.point(x, y);
 
             int move = rand.nextInt(4);
 
-            if(move == 0){
+            if (move == 0) {
                 x++;
-            } else if(move == 1){
+            } else if (move == 1) {
                 x--;
-            } else if(move == 2){
+            } else if (move == 2) {
                 y++;
-            } else if(move == 3) {
+            } else if (move == 3) {
                 y--;
             }
 
             steps++;
 
-            //System.out.println("Position = ("+ x + "," + y +")" );
+            // System.out.println("Position = ("+ x + "," + y +")" );
         }
         System.out.println("Number of steps: " + steps);
     }
@@ -55,14 +54,5 @@ public class Opgave4 {
         }
         return input.nextInt();
     }
-    
+
 }
-
-
-
-    
-
-
-
-
-
