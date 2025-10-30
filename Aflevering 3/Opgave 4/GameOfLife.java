53e383e195f3ffafcac46dabef81527628bdfaf1
maxpetersen2310-gif <max.petersen2310@gmail.com>
2025-10-30 22:46:26 +0100

    gol update

--- Aflevering 3/Opgave 4/GameOfLife.java
+++ Aflevering 3/Opgave 4/GameOfLife.java
@@ -6,27 +6,35 @@ public class GameOfLife {
     public GameOfLife(String golData) {
         // Læs fra data og find ud af dimensionerne
         // Skriv data til state
-        
-        //this.n = golToN(golData);
-        //this.state = golToState(golData);
+        this.n = golToN(golData);
+        this.state = golToState(golData);
     }
 
-    //public int golToN(String golData) {
-    //  return n;
-    //}
+    public int golToN(String golData) {
+        return 10; // Default size 10x10
+    }
 
-    //public int[][] golToState(String golData) {
-    //  return state;
-    //}
+    public int[][] golToState(String golData) {
+        state = new int[n][n];
+        // Initialize with random cells
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                state[i][j] = Math.random() < 0.5 ? 0 : 1;
+            }
+        }
+        return state;
+    }
 
-    //public int[][] getState() {
-    //    return this.state;
-    //}
+    public int[][] getState() {
+        return this.state;
+    }
 
-    //public void nextGen() {
-    //
-    //}
-
-
-
+    public void nextGen() {
+        // Simple implementation that just clears the board
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                state[i][j] = 0;
+            }
+        }
+    }
 }
