cd376fafb37bb4f7348953a96d205c3e217992b6
maxpetersen2310-gif <max.petersen2310@gmail.com>
2025-10-30 14:08:20 +0100

    update start

--- Aflevering 3/Opgave 4/GameOfLife.java
+++ Aflevering 3/Opgave 4/GameOfLife.java
@@ -1,5 +1,12 @@
 package Aflevering 3.Opgave 4;
 
+import java.util.*;
 public class GameOfLife {
-    
+    int n;
+    int[][] state;
+
+    public GameOfLife(int n);
+    if (n<=0) 
+    this.n=n
+    this.state=
 }
