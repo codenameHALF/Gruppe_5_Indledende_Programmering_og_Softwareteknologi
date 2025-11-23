102ad4104376f5a7acd8656535785c067b12be2b
s256111 <s256111@student.dtu.dk>
2025-11-24 00:24:34 +0100

    Added solution to task 1

---
+++ Aflevering 4/Opgave1/Complex.java
@@ -0,0 +1,53 @@
+public class Complex {
+
+    private double re;
+    private double im;
+
+    public Complex() {
+        this.re = 0.0;
+        this.im = 0.0;
+    }
+
+    public Complex(double re, double im) {
+        this.re = re;
+        this.im = im;
+    }
+
+    public Complex(Complex z) {
+        this.re = z.getRe();
+        this.im = z.getIm();
+    }
+
+    public double getRe() {
+        return re;
+    }
+
+    public double getIm() {
+        return im;
+    }
+
+    public double abs() {
+        return Math.sqrt(Math.pow(this.re, 2) + Math.pow(this.im, 2));
+    }
+
+    public Complex plus(Complex other) {
+        double newRe = this.re + other.getRe();
+        double newIm = this.im + other.getIm();
+        return new Complex(newRe, newIm);
+    }
+
+    public Complex times(Complex other) {
+        double newRe = (this.re * other.re) - (this.im * other.im);
+        double newIm = (this.im * other.re) + (this.re * other.im);
+        return new Complex(newRe, newIm);
+    }
+
+    public String toString() {
+        if (this.im >= 0) {
+            return String.valueOf(this.re) + " + " + String.valueOf(this.im) + "i";
+        } else {
+            return String.valueOf(this.re) + " - " + String.valueOf(Math.abs(this.im)) + "i";
+        }
+    }
+
+}
---
+++ Aflevering 4/Opgave1/Mandelbrot.java
@@ -0,0 +1,74 @@
+import java.util.*;
+import java.io.*;
+import java.io.FileNotFoundException;
+import java.awt.*;
+
+
+public class Mandelbrot {
+    
+    public static int MAX = 255;
+    public static int GRIDRES = 512;
+    public static Complex c = new Complex(-0.5, 0.0);
+    public static double s = 2.0;
+    public static Color[] palette;
+    public static String palettePath = "mnd/blues.mnd";
+
+
+    public static void main(String[] args) {
+        palette = readMnd(palettePath);
+        draw();
+    }
+
+    public static Color[] readMnd(String path) {
+        Color[] newPalette = new Color[256];
+        File file = new File(path);
+        try (Scanner scanner = new Scanner(file)) {
+            int i = 0;
+            while (scanner.hasNextInt()) {
+                int r = scanner.nextInt();
+                int g = scanner.nextInt();
+                int b = scanner.nextInt();
+                newPalette[i] = new Color(r, g, b);
+                i++;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return newPalette;
+
+    }
+
+    public static void draw() {
+        StdDraw.setXscale(0,GRIDRES);
+        StdDraw.setYscale(0,GRIDRES);
+        //StdDraw.setCanvasSize(1000,1000);
+        StdDraw.setPenRadius(1.0/GRIDRES);
+        StdDraw.show(0);
+        for (int k = 0; k < GRIDRES; k++) {
+            for (int j = 0; j < GRIDRES; j++) {
+                Complex point = gridComplex(j, k);
+                int iterations = iterate(point);
+                StdDraw.setPenColor(palette[iterations]);
+                StdDraw.point(j+0.5, k+0.5);
+            }
+        }
+        StdDraw.show(0);
+    }
+
+    public static int iterate(Complex z0) {
+        Complex z = new Complex(z0);
+        for (int i = 0; i < MAX; i++) {
+            if (z.abs() > 2.0) {
+                return i;
+            }
+            z = z.times(z).plus(z0);
+        }
+        return MAX;
+    }
+
+    public static Complex gridComplex(int j, int k) {
+        double newRe = c.getRe() - (s / 2) + (s * j) / (GRIDRES - 1);
+        double newIm = c.getIm() - (s / 2) + (s * k) / (GRIDRES - 1);
+        return new Complex(newRe, newIm);
+    }
+}
